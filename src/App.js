29b5e9417a6c4153c2b4a7f19b89b626ccc3d748
dashahello <st.daryaa@gmail.com>
2021-06-26 16:40:51 +0300

    + Redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts.js';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
@@ -7,7 +9,11 @@ import useStyles from './styles.js';
 
 export default function App() {
   const classes = useStyles();
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
   return (
     <Container maxwidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,8 +1,13 @@
+import { useSelector } from 'react-redux';
 import SinglePost from './SinglePost/SinglePost.js';
 import useStyles from './styles.js';
 
 export default function Posts() {
+  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
+
+  console.log(posts);
+
   return (
     <>
       <h1>Posts</h1>
@@ -11,3 +16,4 @@ export default function Posts() {
     </>
   );
 }
+//41
