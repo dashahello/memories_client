e84ac458a682df27b3fc7e77772ba14aa4abfbda
dashahello <st.daryaa@gmail.com>
2021-06-29 10:49:50 +0300

    ^posts && ^style

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,3 +1,4 @@
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import SinglePost from './SinglePost/SinglePost.js';
 import useStyles from './styles.js';
@@ -8,12 +9,21 @@ export default function Posts() {
 
   console.log(posts);
 
-  return (
-    <>
-      <h1>Posts</h1>
-      <SinglePost />
-      <SinglePost />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <SinglePost post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 //41
